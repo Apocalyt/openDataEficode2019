b970a2a1bb6a4662e323f8e5f9ec24137cb18d59
Apocalyt <Apocalyt@gmail.com>
2019-02-10 20:39:26 +0200

    Save open data to postgresql database

--- backend/app.js
+++ backend/app.js
@@ -4,19 +4,42 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const { Client } = require('pg');
+var schedule = require('node-schedule');
+var request = require('request');
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
+const { Pool, Client } = require('pg');
 
+const client = new Client({connectionString: process.env.DATABASE_URL});
 client.connect();
 
-client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
-  console.log(err ? err.stack : res.rows[0].message) // Hello World!
-  client.end()
-})
+const requestOptions = {
+  url: 'https://opendata.hopefully.works/api/events',
+  headers: {
+    'Authorization': 'Bearer ' + process.env.API_TOKEN
+  }
+}
+
+// Write eficode open data to database 
+var j = schedule.scheduleJob('1 * * * *', function(){
+   request(requestOptions, function(error, response, body) {
+     console.log('error: ', error);
+     console.log('statusCode: ', response && response.statusCode);
+     if(!error && response && response.statusCode == 200) {
+       let parsed = JSON.parse(body);
+       let names = ["sensor1", "sensor2", "sensor3", "sensor4"];
+       let values = [parsed["sensor1"], parsed["sensor2"], parsed["sensor3"], parsed["sensor4"]];
+       console.log(names);
+       console.log(values);
+       for(let i = 0; i < names.length; i++) {
+         let params = [names[i],values[i],parsed["date"]];
+         client.query('INSERT INTO data(sensor_name, value, value_ts) VALUES ($1,$2,$3) RETURNING *;',params, (err, res) => {
+           console.log(err ? err.stack : res.rows);         
+         });
+       }
+     }
+  });
+});
+
 
 //var indexRouter = require('./routes/index');
 var dataRouter = require('./routes/data');
--- database/01_create_db_schema.sql
+++ database/01_create_db_schema.sql
@@ -1,7 +1,8 @@
 CREATE TABLE data(
-	sensor_name TEXT PRIMARY KEY,
+	id BIGSERIAL PRIMARY KEY,
+	sensor_name TEXT,
 	value double precision,
 	value_ts timestamp
 )
-
+CREATE INDEX name_index ON data(sensor_name);
 CREATE INDEX time_index ON data(value_ts);
